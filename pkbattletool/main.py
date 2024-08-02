f1e2e2ba20fcc28ed5adde61d93268addf72b806
ando1912 <c1007167@planet.kanazawa-it.ac.jp>
2024-08-03 02:54:16 +0900

    Update main.py
    ウィンドウ終了時の処理の追加

--- pkbattletool/main.py
+++ pkbattletool/main.py
@@ -5,12 +5,15 @@ from logging import getLogger, StreamHandler, INFO, DEBUG, Formatter, FileHandle
 from mylib import OcrRunner, CameraCapture
 from gui import MenuBar, PKInfo, PkInfo2, CanvasGame, CaptureControl, CanvasPkBox
 
+import datetime
+
 PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 def gui_main():
     # TODO: ウィンドウを開く位置を前回の位置にしたい
     root = tk.Tk()
-    width = 1400 #660
+    root.withdraw() # ウィンドウを非表示にする
+    width = 1400
     height = 900
     
     # ウィンドウタイトル
@@ -22,31 +25,37 @@ def gui_main():
     camera_capture = CameraCapture()
 
     # フレームウェジットの作成
-    """
-    frame_pkinfo        : 
-    frame_canvasgame    :ゲーム画面を映すフレーム
-    frame_capturecontrol:キャプチャコントロールのフレーム
-    frame_canvaspkbox   :相手の手持ちポケモンリストを表示するフレーム
-    """
+    # ポケモン情報簡易表示
     frame_pkinfo = PkInfo2(root, camera_capture, bd=1, relief=tk.SOLID)
     frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
     
+    # ゲーム画面
     frame_canvasgame = CanvasGame(root, camera_capture, bd=2, relief=tk.SOLID)
     frame_canvasgame.grid(row=1, rowspan=2, column=0, columnspan=2, padx=5, pady=5)
     
+    # キャプチャコントロール
     frame_capturecontrol = CaptureControl(root, camera_capture, bd=1, relief=tk.SOLID)
     frame_capturecontrol.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
     
+    # 相手の手持ち一覧
     frame_canvaspkbox = CanvasPkBox(root, camera_capture, bd=2, relief=tk.SOLID)
     frame_canvaspkbox.grid(row=0, column=2, rowspan=3, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
 
+    # ウィンドウを閉じる時の処理
     def click_close():
         frame_capturecontrol.camera_control.stop_capture()
         frame_capturecontrol.camera_control.release_camera()
+        frame_pkinfo.ocr_control["level"].stop_ocr_thread()
+        frame_pkinfo.ocr_control["namebox"].stop_ocr_thread()
+        getLogger("Log").getChild("GUImain").info("Process end")
+        root.quit()
         root.destroy()
-
+        
     root.protocol("WM_DELETE_WINDOW", click_close)
 
+    frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
+    
+    root.deiconify() # ウィンドウを再表示する
     root.mainloop()
 
 def main():
@@ -54,23 +63,25 @@ def main():
     logger = getLogger("Log")
     handler = StreamHandler()
 
-    LOG_LEVEL = DEBUG
+
+    LOGGER_LEVEL = DEBUG
+    HANDLER_LEVEL = DEBUG
     
-    logger.setLevel(LOG_LEVEL)
-    handler.setLevel(LOG_LEVEL)
+    logger.setLevel(LOGGER_LEVEL)
+    handler.setLevel(HANDLER_LEVEL)
     formatter = Formatter('%(asctime)s | %(levelname)s | %(name)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    log_path = f"{PATH}/log.txt"
-    if os.path.exists(log_path):
-        os.remove(log_path)
+    if not os.path.exists(f"{PATH}/log/"):
+        os.mkdir(f"{PATH}/log")
+    log_path = f"{PATH}/log/log_{datetime.datetime.now().strftime('%y%m%d%H%M%S')}.txt"
     fh = FileHandler(filename=log_path)
-    fh.setLevel(LOG_LEVEL)
+    fh.setLevel(LOGGER_LEVEL)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
-    logger.info("Start PkBattleTool !!")
+    logger.info("START PkBattleTool")
 
     gui_main()
 
