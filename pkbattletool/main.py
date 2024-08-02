3deebad99aba111ac8494aed33a9bdfdba741623
ando1912 <c1007167@planet.kanazawa-it.ac.jp>
2024-08-02 17:59:01 +0900

    Update main.py
    細部更新

--- pkbattletool/main.py
+++ pkbattletool/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os,sys
-from logging import getLogger, StreamHandler, DEBUG, Formatter, FileHandler
+from logging import getLogger, StreamHandler, INFO, DEBUG, Formatter, FileHandler
 
 from mylib import OcrRunner, CameraCapture
 from gui import MenuBar, PKInfo,PkInfo2, CanvasGame, CaptureControl, CanvasPkBox
@@ -20,8 +20,6 @@ def gui_main():
     menuber = MenuBar(root)
     
     camera_capture = CameraCapture()
-    namebox_ocr_runner = OcrRunner(camera_capture, ocr_option="namebox")
-    # pokemonbox_ocr_runner = OcrRunner(camera_capture, ocr_option="pokemonbox")
 
     # フレームウェジットの作成
     """
@@ -30,16 +28,16 @@ def gui_main():
     frame_capturecontrol:キャプチャコントロールのフレーム
     frame_canvaspkbox   :相手の手持ちポケモンリストを表示するフレーム
     """
-    # frame_pkinfo = PKInfo(root, camera_capture, bd=1, relief=tk.SOLID)
-    frame_pkinfo = PkInfo2(root, namebox_ocr_runner, bd=1, relief=tk.SOLID)
-    frame_canvasgame = CanvasGame(root, camera_capture, bd=2, relief=tk.SOLID)
-    frame_capturecontrol = CaptureControl(root, camera_capture, bd=1, relief=tk.SOLID)
-    frame_canvaspkbox = CanvasPkBox(root, camera_capture, bd=2, relief=tk.SOLID)
-
-    # ウェジットの配置
+    frame_pkinfo = PkInfo2(root, camera_capture, bd=1, relief=tk.SOLID)
     frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
+    
+    frame_canvasgame = CanvasGame(root, camera_capture, bd=2, relief=tk.SOLID)
     frame_canvasgame.grid(row=1, rowspan=2, column=0, columnspan=2, padx=5, pady=5)
+    
+    frame_capturecontrol = CaptureControl(root, camera_capture, bd=1, relief=tk.SOLID)
     frame_capturecontrol.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
+    
+    frame_canvaspkbox = CanvasPkBox(root, camera_capture, bd=2, relief=tk.SOLID)
     frame_canvaspkbox.grid(row=0, column=2, rowspan=3, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
 
     def click_close():
@@ -56,18 +54,23 @@ def main():
     logger = getLogger("Log")
     handler = StreamHandler()
 
-    logger.setLevel(DEBUG)
-    handler.setLevel(DEBUG)
-    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    LOG_LEVEL = INFO
+    
+    logger.setLevel(LOG_LEVEL)
+    handler.setLevel(LOG_LEVEL)
+    formatter = Formatter('%(asctime)s | %(levelname)s | %(name)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    fh = FileHandler(filename=f"{PATH}/log.txt")
-    fh.setLevel(DEBUG)
+    log_path = f"{PATH}/log.txt"
+    if os.path.exists(log_path):
+        os.remove(log_path)
+    fh = FileHandler(filename=log_path)
+    fh.setLevel(LOG_LEVEL)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
-    logger.debug("Hello main")
+    logger.info("Start PkBattleTool !!")
 
     gui_main()
 
