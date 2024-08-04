835c274cf4399a4441ae1732a12f9ba868a7af03
ando1912 <c1007167@planet.kanazawa-it.ac.jp>
2024-08-04 13:37:42 +0900

    Update main.py
    OcrControlクラスの廃止

--- pkbattletool/main.py
+++ pkbattletool/main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import os,sys
 from logging import getLogger, StreamHandler, INFO, DEBUG, Formatter, FileHandler
 
-from mylib import OcrRunner, CameraCapture, OcrControl
+from mylib import OcrRunner, CameraCapture
 from gui import MenuBar, PkInfo_OCR, CanvasGame, CaptureControl, CanvasPkBox
 
 import datetime
@@ -29,11 +29,12 @@ def gui_main():
     menuber = MenuBar(root)
     
     camera_capture = CameraCapture()
-    ocr_control = OcrControl(OcrRunner(camera_capture))
+    ocr_runner = OcrRunner(camera_capture)
+    # ocr_control = OcrControl(OcrRunner(camera_capture))
 
     # フレームウェジットの作成
     # ポケモン情報簡易表示
-    frame_pkinfo = PkInfo_OCR(root, ocr_control, bd=1, relief=tk.SOLID)
+    frame_pkinfo = PkInfo_OCR(root, ocr_runner, bd=1, relief=tk.SOLID)
     frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
     
     # ゲーム画面
@@ -41,19 +42,19 @@ def gui_main():
     frame_canvasgame.grid(row=1, rowspan=2, column=0, columnspan=2, padx=5, pady=5)
     
     # キャプチャコントロール
-    frame_capturecontrol = CaptureControl(root, camera_capture, ocr_control, bd=1, relief=tk.SOLID)
+    frame_capturecontrol = CaptureControl(root, camera_capture, ocr_runner, bd=1, relief=tk.SOLID)
     frame_capturecontrol.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
     
     # 相手の手持ち一覧
-    frame_canvaspkbox = CanvasPkBox(root, camera_capture, ocr_control, bd=2, relief=tk.SOLID)
+    frame_canvaspkbox = CanvasPkBox(root, camera_capture, ocr_runner, bd=2, relief=tk.SOLID)
     frame_canvaspkbox.grid(row=0, column=2, rowspan=3, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
 
     # ウィンドウを閉じる時の処理
     def click_close():
         frame_capturecontrol.camera_control.stop_capture()
         frame_capturecontrol.camera_control.release_camera()
-        frame_pkinfo.ocr_control.stop_ocr_thread()
-        frame_canvaspkbox.ocr_control.stop_ocr_thread()
+        frame_pkinfo.ocr_runner.stop_ocr_thread()
+        frame_canvaspkbox.ocr_runner.stop_ocr_thread()
         getLogger("Log").getChild("GUImain").info("Process end")
         root.quit()
         root.destroy()
