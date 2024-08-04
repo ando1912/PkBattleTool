0907d14a397cafe525d8a5d585e2f4d308ef8671
ando1912 <c1007167@planet.kanazawa-it.ac.jp>
2024-08-04 13:39:14 +0900

    Update webcam_capture.py

--- pkbattletool/mylib/webcam_capture.py
+++ pkbattletool/mylib/webcam_capture.py
@@ -1,65 +1,66 @@
 import os, sys
 import cv2
-from PIL import Image, ImageTk
-import threading
+from threading import Thread
 import datetime
 from logging import getLogger
 
 
 from module import config
 PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
+
 class CameraCapture:
     def __init__(self) -> None:
         self.logger = getLogger("Log").getChild("CameraCapture")
 
-        self.video_source = int(config.get("DEFAULT","camera_id"))
+        self.video_source:int = int(config.get("DEFAULT","camera_id"))
         self.vid = cv2.VideoCapture(self.video_source)
-
-        self.screenshot_folder_path = config.get("DEFAULT","screenshot_folder")
-        self.display_fps = config.get("DEFAULT","display_fps")
 
         self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        
+        self.screenshot_folder_path = config.get("DEFAULT","screenshot_folder")
+        
+        
+        self.vid.set(cv2.CAP_PROP_FPS, int(config.get("DEFAULT","display_fps")))
 
-        self.width = int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.logger.debug(f"CameraConfig:{self.width}x{self.height}/{self.display_fps}")
-        # print("W:{} / H:{}".format(self.width,self.height))
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.display_fps = self.vid.get(cv2.CAP_PROP_FPS)
+        
+        self.logger.info(f"CameraConfig : w{self.width:.2f} x h{self.height:.2f} / {self.display_fps:.2f}fps")
 
-        self.is_capturing = False
-        self.capture_thread = None
+        self.is_capturing:bool = False
+        self.capture_thread:Thread = None
         self.frame:cv2.typing.MatLike = None
     
-    def __del__(self) -> None:
-        self.stop_capture()
-        if self.vid.isOpened():
-            self.vid.release()
-
-
     def start_capture(self) -> None:
         """
         カメラの撮影を開始する
         """
-        self.logger.info("Execute start_capture")
+        self.logger.getChild("start_capture").info("Execute start_capture")
         self.is_capturing= True
-        self.capture_thread = threading.Thread(target=self.capture_video)
+        self.capture_thread = Thread(target=self.capture_video)
+        self.capture_thread.daemon = True
         self.capture_thread.start()
     
     def stop_capture(self) -> None:
         """
         カメラの撮影を停止する
         """
-        self.logger.info("Execute stop_capture")
+        self.logger.getChild("stop_capture").info("Execute stop_capture")
         self.is_capturing = False
     
     def capture_video(self) -> None:
         """
         モードが有効の間、撮影したフレームを格納する
         """
-        self.logger.info("Execute capture_video")
+        logger = self.logger.getChild("capture_video")
+        logger.info("Execute capture_video")
         while self.is_capturing:
+            logger.debug("VideoCapture.read")
             ret, frame = self.vid.read()
             if ret:
+                logger.debug("save frame")
                 self.frame = frame
         
     def get_frame(self) -> tuple[bool, cv2.typing.MatLike]:
@@ -70,7 +71,7 @@ class CameraCapture:
         """
         
         
-        return self.vid.read()
+        return self.frame is not None, self.frame
 
     def save_frame(self) -> None:
         """
@@ -82,14 +83,14 @@ class CameraCapture:
             if os.path.exists(f"{PATH}/{self.screenshot_folder_path}"):
                 os.mkdir(f"{PATH}/{self.screenshot_folder_path}")
                 self.logger.info(f"Success makedir {self.screenshot_folder_path}")
-        except:
-            self.logger.error(f"Fault makedir {self.screenshot_folder_path}")
+        except Exception as e:
+            self.logger.error(f"Fault makedir {self.screenshot_folder_path} : {e}")
         file_name = f"screenshot_{datetime.datetime.now().strftime('%y%m%d%H%M%S')}"
         try:
             cv2.imwrite(f"{PATH}/{self.screenshot_folder_path}/{file_name}.png", self.frame)
             print("save as {}".format(file_name))
-        except:
-            self.logger.error("Fault save image")
+        except Exception as e:
+            self.logger.error(f"Fault save image : {e}")
 
     def release(self) -> None:
         """
