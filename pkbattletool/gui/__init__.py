08a6a240689ebc356aed07911ff56fdac240167a
ando1912 <c1007167@planet.kanazawa-it.ac.jp>
2024-08-04 17:58:36 +0900

    Update __init__.py

--- pkbattletool/gui/__init__.py
+++ pkbattletool/gui/__init__.py
@@ -1,10 +1,3 @@
 # gui/__init__.py
-from .frame import PkInfo_OCR
-from .frame import CanvasGame
-from .frame import CaptureControl
-from .frame import CanvasPkBox
-
-from .menu import MenuBar
-from .menu import SubWindowMenu
-
-# from .subwindow import SubWindow
+from .frame import *
+from .menu import *
