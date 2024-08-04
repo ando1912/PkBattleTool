831995417961bd6f458b68dca6a7b789fdb4a11a
ando1912 <c1007167@planet.kanazawa-it.ac.jp>
2024-08-04 18:52:13 +0900

    Update main.py
    メインウィンドウ処理をクラス化

--- pkbattletool/main.py
+++ pkbattletool/main.py
@@ -9,61 +9,44 @@ import datetime
 
 PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-def gui_main():
-    # TODO: ウィンドウを開く位置を前回の位置にしたい
-    root = tk.Tk()
-    root.withdraw() # ウィンドウを非表示にする
-    
-    # アイコンの設定
-    # 参考；https://qiita.com/KMiura95/items/cae599efa8a908a4bb01
-    root.iconbitmap(
-        default=f"{PATH}/resources/icon.ico"
-    )
-    
-    root.resizable(0,0) # ウィンドウサイズ変更の禁止
-    
-    # ウィンドウタイトル
-    root.title("ポケモン対戦支援ツール")
-
-    # メニューバーの設定
-    menuber = MenuBar(root)
-    
-    camera_capture = CameraCapture()
-    ocr_runner = OcrRunner(camera_capture)
-
-    # フレームウェジットの作成
-    # ポケモン情報簡易表示
-    frame_pkinfo = PkInfo_OCR(root, ocr_runner, bd=1, relief=tk.SOLID)
-    frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
-    
-    # ゲーム画面
-    frame_canvasgame = CanvasGame(root, camera_capture, bd=2, relief=tk.SOLID)
-    frame_canvasgame.grid(row=1, rowspan=2, column=0, columnspan=2, padx=5, pady=5)
-    
-    # キャプチャコントロール
-    frame_capturecontrol = CaptureControl(root, camera_capture, ocr_runner, bd=1, relief=tk.SOLID)
-    frame_capturecontrol.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
-    
-    # 相手の手持ち一覧
-    frame_canvaspkbox = CanvasPkBox(root, camera_capture, ocr_runner, bd=2, relief=tk.SOLID)
-    frame_canvaspkbox.grid(row=0, column=2, rowspan=3, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
-
-    # ウィンドウを閉じる時の処理
-    def click_close():
-        frame_capturecontrol.camera_capture.stop_capture()
-        frame_capturecontrol.camera_capture.release_camera()
-        frame_pkinfo.ocr_runner.stop_ocr_thread()
-        frame_canvaspkbox.ocr_runner.stop_ocr_thread()
-        getLogger("Log").getChild("GUImain").info("Process end")
-        root.quit()
-        root.destroy()
+class MainWindow(tk.Frame):
+    def __init__(self, root:tk.Tk=None):
+        super().__init__(root)
+        menubar = MenuBar(root)
         
-    root.protocol("WM_DELETE_WINDOW", click_close)
+        camera_capture = CameraCapture()
+        ocr_runner = OcrRunner(camera_capture)
 
-    frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
-    
-    root.deiconify() # ウィンドウを再表示する
-    root.mainloop()
+        # フレームウェジットの作成
+        # ポケモン情報簡易表示
+        frame_pkinfo = PkInfo_OCR(root, ocr_runner, bd=1, relief=tk.SOLID)
+        frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
+        
+        # ゲーム画面
+        frame_canvasgame = CanvasGame(root, camera_capture, bd=2, relief=tk.SOLID)
+        frame_canvasgame.grid(row=1, rowspan=2, column=0, columnspan=2, padx=5, pady=5)
+        
+        # キャプチャコントロール
+        frame_capturecontrol = CaptureControl(root, camera_capture, ocr_runner, bd=1, relief=tk.SOLID)
+        frame_capturecontrol.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
+        
+        # 相手の手持ち一覧
+        frame_canvaspkbox = CanvasPkBox(root, camera_capture, ocr_runner, bd=2, relief=tk.SOLID)
+        frame_canvaspkbox.grid(row=0, column=2, rowspan=3, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
+
+        # ウィンドウを閉じる時の処理
+        def click_close():
+            frame_capturecontrol.camera_capture.stop_capture()
+            frame_capturecontrol.camera_capture.release_camera()
+            frame_pkinfo.ocr_runner.stop_ocr_thread()
+            frame_canvaspkbox.ocr_runner.stop_ocr_thread()
+            getLogger("Log").getChild("GUImain").info("Process end")
+            root.quit()
+            root.destroy()
+            
+        root.protocol("WM_DELETE_WINDOW", click_close)
+
+        frame_pkinfo.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
 
 def main():
     # ログ設定
@@ -89,8 +72,23 @@ def main():
     logger.addHandler(fh)
 
     logger.info("START PkBattleTool")
-
-    gui_main()
+    
+    root = tk.Tk()
+    root.withdraw() # ウィンドウを非表示にする
+    
+    # アイコンの設定
+    # 参考；https://qiita.com/KMiura95/items/cae599efa8a908a4bb01
+    root.iconbitmap(
+        default=f"{PATH}/resources/icon.ico"
+    )
+    
+    root.resizable(0,0) # ウィンドウサイズ変更の禁止
+    
+    # ウィンドウタイトル
+    root.title("ポケモン対戦支援ツール")
+    window = MainWindow(root)
+    window.deiconify() # ウィンドウを再表示する
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
